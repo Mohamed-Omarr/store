7e52e9738a2bf8c5268f2fcb55fb8cb59b6d7f80
Mohamed Omar <mohombusiness@gmail.com>
2025-07-06 00:31:59 +0300

    Add files via upload

--- src/app/utils/fetchGold.ts
+++ src/app/utils/fetchGold.ts
@@ -1,24 +1,31 @@
-// Helper funtions
 export async function getGoldPricePerGram(): Promise<number | null> {
   try {
     const res = await fetch("/api/getGold");
     if (!res.ok) throw new Error("Failed to fetch gold price");
     const json = await res.json();
     return parseFloat(json.pricePerGram);
-  } catch (error) {
-    return null;
+  } catch (error: unknown) {
+    console.error(error)
+    if (error instanceof Error) {
+      throw new Error(`Failed to fetch gold price: ${error.message}`);
+    }
+    throw new Error("Failed to fetch gold price: Unknown error");
   }
 }
 
 export async function getLocalProduct(): Promise<Product[] | null> {
-    try {
-      const res = await fetch("/localJson/products.json");
-      if (!res.ok) {
-        throw new Error(`HTTP Status Error: ${res.status}`);
-      }
-      const data = await res.json();
-      return data;
-    } catch (error) {
-      throw new Error(`Failed to fetch products: ${error}`);
-    } 
+  try {
+    const res = await fetch("/localJson/products.json");
+    if (!res.ok) {
+      throw new Error(`HTTP Status Error: ${res.status}`);
+    }
+    const data = await res.json();
+    return data;
+  } catch (error: unknown) {
+    console.error(error)
+    if (error instanceof Error) {
+      throw new Error(`Failed to fetch products: ${error.message}`);
+    }
+    throw new Error("Failed to fetch products: Unknown error");
+  }
 }
